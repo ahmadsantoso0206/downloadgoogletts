44b13c845a677fc6ed2e94fcbec58e1e069912bf
ahmadsantoso0206 <31234915+ahmadsantoso0206@users.noreply.github.com>
2017-10-03 15:29:57 +0700

    Update main.js

--- scripts/public/js/main.js
+++ scripts/public/js/main.js
@@ -1,7 +1,7 @@
 
 
 $(document).ready(function(){
-    var socket = io.connect('https://secure-garden-93788.herokuapp.com:3000'); 
+    var socket = io.connect(); 
     $("#btnDownload").addClass('disabled');
     // var id = socket.io.engine.id;
     // alert(  id); 
